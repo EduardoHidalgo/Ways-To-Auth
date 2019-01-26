e33e5d30a305a2189b3e0a0ddb8caa155df7a675
Eduardo Hidalgo <IngEduardoHidalgo@hotmail.com>
2019-01-26 12:40:53 -0500

    scripts de facebook como proveedor de login

--- pages/_app.js
+++ pages/_app.js
@@ -29,6 +29,24 @@ class MyApp extends App {
     if (jssStyles && jssStyles.parentNode) {
       jssStyles.parentNode.removeChild(jssStyles);
     }
+
+    /* facebook script */
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId      : '2216928228374306',
+        cookie     : true,
+        xfbml      : true,
+        version    : 'v3.2'
+      });
+      FB.AppEvents.logPageView();   
+    };
+    (function(d, s, id){
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {return;}
+      js = d.createElement(s); js.id = id;
+      js.src = "https://connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
   }
 
   render() {
