cdde2b34627bbcf62835d99b3ac4c8d7e1ecd1ab
Eduardo Hidalgo <IngEduardoHidalgo@hotmail.com>
2019-01-30 10:49:34 -0500

    esqueleto del componente

---
+++ src/components/firebase/useGetMessages.js
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+import 'isomorphic-unfetch';
+import clientCredentials from '../../credentials/client';
+
+function useGetMessages(props) {
+ return
+}
+
+export default useGetMessages;
